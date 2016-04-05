559d9764f9df0ff61319f68ef9496305573f70f7
Kurt Huwig <kurthuwig@gmail.com>
2016-04-05 15:49:00 +0200

    Bug: config files are not created when they do not exist

--- files/entrypoint.sh
+++ files/entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-test -e /etc/ganglia/gmond.conf  && cp /gmond.conf.default  /etc/ganglia/gmond.conf
-test -e /etc/ganglia/gmetad.conf && cp /gmetad.conf.default /etc/ganglia/gmetad.conf
+test -e /etc/ganglia/gmond.conf  || cp /gmond.conf.default  /etc/ganglia/gmond.conf
+test -e /etc/ganglia/gmetad.conf || cp /gmetad.conf.default /etc/ganglia/gmetad.conf
 
 mkdir -p /var/lib/ganglia/rrds
 chown nobody /var/lib/ganglia/rrds
